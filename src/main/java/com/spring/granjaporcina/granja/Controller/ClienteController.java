c61e01c934a3adf0e17014f185f6859d426fd207
León Ángel Chancí Guzmán <leonchanci@gmail.com>
2024-10-28 15:51:37 -0500

    sussesfully
    sussesfully

--- src/main/java/com/spring/granjaporcina/granja/Controller/ClienteController.java
+++ src/main/java/com/spring/granjaporcina/granja/Controller/ClienteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("cliente")
+@RequestMapping("/cliente")
 public class ClienteController {
 
     private final ClienteService clienteService;
